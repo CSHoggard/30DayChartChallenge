e8026b0251127876d1b56bd117f05a8aae65a047
CSHoggard <christianhoggard@gmail.com>
2021-04-03 22:38:13 +0100

    finalising

--- historical.R
+++ historical.R
@@ -6,8 +6,8 @@ library(extrafont)
 library(ggtext)
 library(here)
 
-Sys.setenv(SPOTIFY_CLIENT_ID = 'XXXXXXXXXXXXXXXXXXXXXXXX')
-Sys.setenv(SPOTIFY_CLIENT_SECRET = 'XXXXXXXXXXXXXXXXXXXXXXXX')
+Sys.setenv(SPOTIFY_CLIENT_ID = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX')
+Sys.setenv(SPOTIFY_CLIENT_SECRET = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX')
 
 access_token <- get_spotify_access_token()
 
@@ -21,18 +21,22 @@ fleetwood_clean <- fleetwood %>%
   pivot_longer(!album_release_year, names_to = "factor", values_to = "count")
 
 ggplot(fleetwood_clean, aes(album_release_year, count, colour = factor)) +
-  geom_point(size = 4) +
-  geom_bump(size = 1.5,
+  geom_point(size = 5) +
+  geom_bump(size = 2,
             smooth = 7) +
   lims(y = c(0,1)) +
-  labs(title = "Deconstructing Fleetwood Mac Albums",
-       subtitle = "Investigating <span style = 'color:#E1C844;'> acousticness</span>, <span style = 'color:#003008;'>danceability</span>, <span style = 'color:#003008;'>energy</span> and <span style = 'color:#7BA487;'>musical positivity</span>",
-       caption = "Data: Spotify Web API (courtesy of spotifyr) | @CSHoggard",
+  labs(title = "Don't Stop (Thinking About Data Visualisation)",
+       subtitle = "Fleetwood Mac album <span style = 'color:#E1C844;'> acousticness</span>, <span style = 'color:#003008;'>danceability</span>, <span style = 'color:#A2A475;'>energy</span> and <span style = 'color:#7BA487;'>musical positivity</span>",
+       caption = "Data: Spotify Web API (courtesy of spotifyr) | #30DayChartChallenge | @CSHoggard",
        y = "Index") +
   scale_color_manual(values = wes_palette(n = 4, name = "Cavalcanti1")) + 
   theme_minimal(base_family = "Asap Condensed") +
   theme(plot.margin = margin(20,20,20,20),
-        plot.subtitle = element_textbox_simple(colour = "grey10", family = "IBM Plex Sans", size = 11),
+        plot.title = element_text(size = 22),
+        plot.subtitle = element_textbox_simple(colour = "grey10", size = 14),
+        plot.caption = element_text(hjust = 0, colour = "grey40", margin = margin(20,0,5,0)),
         axis.title.x = element_blank(),
+        axis.title.y = element_text(size = 12, margin = margin(0,20,0,0)),
         legend.position = "none")
-  
+
+ggsave("30DayChartChallenge/Historic.png", plot = last_plot(), width = 300, height = 140, units = "mm", dpi = 400)  
